2928942ce5fd7a880d8dc05472cc8f057742b759
bjkillen <blaine.j.killen@gmail.com>
2024-11-13 22:20:47 -0800

    updated stratagem question to follow stratagem effect structure

--- src/StratagemQuestion.ts
+++ src/StratagemQuestion.ts
@@ -1,4 +1,6 @@
 import { jsonObject, jsonMember } from "typedjson";
+import DiceSkillValue from "./enums/DiceSkillValue";
+import DiceRerollModifierValue from "./enums/DiceRerollModifierValue";
 
 @jsonObject
 class StratagemQuestion {
@@ -8,17 +10,97 @@ class StratagemQuestion {
     @jsonMember
     public questionText: string;
 
+        @jsonMember
+    public sustainedHits?: number;
+
     @jsonMember
-    public modifier?: string;
+    public criticalHits?: DiceSkillValue;
+
+    @jsonMember
+    public criticalWounds?: DiceSkillValue;
+
+    @jsonMember
+    public minusOneDamage?: boolean;
+
+    @jsonMember
+    public feelNoPain?: DiceSkillValue;
+
+    @jsonMember
+    public toWoundPlusOne?: boolean;
+
+    @jsonMember
+    public rerollHits?: DiceRerollModifierValue;
+
+    @jsonMember
+    public bonusAttacks?: number;
+
+    @jsonMember
+    public stealth?: boolean;
+
+    @jsonMember
+    public bonusAP?: number;
+
+    @jsonMember
+    public bonusStrength?: number;
+
+    @jsonMember
+    public setWeaponSkill?: DiceSkillValue;
+
+    @jsonMember
+    public toHitPlusOne?: boolean;
+
+    @jsonMember
+    public setInvulnerableSkill?: DiceSkillValue;
+
+    @jsonMember
+    public toWoundMinusOne?: boolean;
+
+    @jsonMember
+    public lethalHits?: boolean;
+
+    @jsonMember
+    public reducesAP: number;
 
     constructor(
         stratagemID: string,
         questionText: string,
-        modifier: string
+        sustainedHits: number | undefined,
+        criticalHits: DiceSkillValue | undefined,
+        criticalWounds: DiceSkillValue | undefined,
+        minusOneDamage: boolean | undefined,
+        feelNoPain: DiceSkillValue | undefined,
+        toWoundPlusOne: boolean,
+        rerollHits: DiceRerollModifierValue | undefined,
+        bonusAttacks: number | undefined,
+        stealth: boolean | undefined,
+        bonusAP: number | undefined,
+        bonusStrength: number | undefined,
+        setWeaponSkill: DiceSkillValue | undefined,
+        toHitPlusOne: boolean | undefined,
+        setInvulnerableSkill: DiceSkillValue | undefined,
+        toWoundMinusOne: boolean | undefined,
+        lethalHits: boolean | undefined,
+        reducesAP: number,
     ) {
         this.stratagemID = stratagemID;
         this.questionText = questionText;
-        this.modifier = modifier;
+        this.sustainedHits = sustainedHits;
+        this.criticalHits = criticalHits;
+        this.criticalWounds = criticalWounds;
+        this.minusOneDamage = minusOneDamage;
+        this.feelNoPain = feelNoPain;
+        this.toWoundPlusOne = toWoundPlusOne;
+        this.rerollHits = rerollHits;
+        this.bonusAttacks = bonusAttacks;
+        this.stealth = stealth;
+        this.bonusAP = bonusAP;
+        this.bonusStrength = bonusStrength;
+        this.setWeaponSkill = setWeaponSkill;
+        this.toHitPlusOne = toHitPlusOne;
+        this.setInvulnerableSkill = setInvulnerableSkill;
+        this.toWoundMinusOne = toWoundMinusOne;
+        this.lethalHits = lethalHits;
+        this.reducesAP = reducesAP;
     }
 }
 
